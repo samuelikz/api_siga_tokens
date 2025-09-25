efcaa7acf2f19fac0e21f758fd2640ec60f4b5f5
Samuel Nunes da Silva <samuel.nuness@ad.lan.perpart>
2025-09-25 11:26:30 -0300

    users add

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,30 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, ForbiddenException, Get, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ListUsersQuery } from './dto/list-users.query/list-users.query';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth/jwt-auth.guard';
+import { CreateUserDto } from './dto/create-user-dto/create-user.dto';
+import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
 @UseGuards(JwtAuthGuard)
+@ApiTags('users')
+@ApiBearerAuth('bearer')
 export class UsersController {
   constructor(private users: UsersService) {}
+
   @Get()
   list(@Query() q: ListUsersQuery) {
     return this.users.list(q.page!, q.pageSize!);
+  }
+
+  /** Criar usuário (apenas ADMIN) */
+  @Post()
+  @ApiBody({ type: CreateUserDto })
+  async create(@Body() dto: CreateUserDto, @Req() req: any) {
+    const role = (req?.user?.role as 'ADMIN' | 'USER' | undefined) ?? 'USER';
+    if (role !== 'ADMIN') {
+      throw new ForbiddenException('Apenas ADMIN pode criar usuários.');
+    }
+    return this.users.create(dto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaPrimaryService } from '../prisma/prisma-primary.service';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user-dto/create-user.dto';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaPrimaryService) {}
+  constructor(private prisma: PrismaPrimaryService) { }
 
   async list(page = 1, pageSize = 20) {
     const take = pageSize;
@@ -19,5 +22,29 @@ export class UsersService {
     ]);
 
     return { page, pageSize: take, total, items };
+  }
+
+  async create(dto: CreateUserDto) {
+    // Verifica e-mail único
+    const exists = await this.prisma.user.findUnique({ where: { email: dto.email } });
+    if (exists) throw new ConflictException('E-mail já cadastrado.');
+
+    // Hash de senha
+    const passwordHash = await bcrypt.hash(dto.password, 10);
+
+    // OBS: ajuste o campo abaixo se no seu schema o nome for diferente (ex.: password, hash, etc.)
+    const user = await this.prisma.user.create({
+      data: {
+        id: randomUUID(),            // mantenha se o schema não tiver default(uuid())
+        email: dto.email,
+        name: dto.name,
+        role: dto.role ?? 'USER',
+        password: passwordHash,      
+        updatedAt: new Date(),       
+      },
+      select: { id: true, email: true, name: true, role: true, createdAt: true },
+    });
+
+    return user;
   }
 }
